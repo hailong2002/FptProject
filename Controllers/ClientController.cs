c26e1e69bd981282bfea8a59413add15c4f38f53
nhtminhh <minhtngch200030@fpt.edu.vn>
2022-10-23 10:44:08 +0700

    ok2

--- Controllers/ClientController.cs
+++ Controllers/ClientController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FPTBook.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace FPTBook.Controllers
 {
+
     public class ClientController : Controller
     {
+        public readonly ApplicationDbContext context;
 
-      
+        public ClientController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,6 +27,18 @@ namespace FPTBook.Controllers
         public IActionResult Blog()
         {
             return View();
+        }
+
+        public IActionResult Store()
+        {
+            return View(context.Books.ToList());
+        }
+
+        public IActionResult Detail(int id)
+        {
+            var cate = context.Categories.ToList();
+            var books = context.Books.Find(id);
+            return View(books);
         }
 
     }
