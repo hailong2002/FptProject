f90ad7efb4a3fb68cf9a65960e0c275649a35311
hailong2002 <103204476+hailong2002@users.noreply.github.com>
2022-10-27 02:02:39 +0700

    27/10 long

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,7 +1,9 @@
 ﻿using FPTBook.Data;
 using FPTBook.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Linq;
+using System.Net;
 
 namespace FPTBook.Controllers
 {
@@ -15,19 +17,52 @@ namespace FPTBook.Controllers
             this.context = context;
         }
 
-        public IActionResult MakeOrder(int id, int quantity)
+        [Authorize (Roles = "Customer")]
+        public IActionResult MakeOrder(int id, int quantity, string customerName)
         {
             var order = new Order();
             order.OrderQuantity = quantity;
             order.BookId = id;
-            //TempData["alo"] = context.Books.Find(id).Title;
+            order.Customer = customerName;
             order.OrderName = context.Books.Find(id).Title;
             order.OrderPrice = (context.Books.Find(id).Price)*quantity;
             context.Orders.Add(order);
-            context.Books.Find(id).Quantity -= quantity; 
             context.SaveChanges();
-           
             return View(order);
+        }
+
+        [Authorize(Roles = "BookOwner")]
+        public IActionResult Record()
+        {
+            var customerOrder = context.Orders.ToList();
+            return View(customerOrder);
+        }
+
+        [Authorize (Roles = "BookOwner")]
+        public IActionResult Approve(int id, int quantity, int bookId)
+        {
+            TempData["approve"] = "Order has been approved";
+            var order = context.Orders.Find(id);
+            //get name of book order
+            var name = context.Orders.Where(x=>x.Id == id).First().OrderName;
+            //decrease quantity of book in db
+            context.Books.Where(x => x.Title == name).First().Quantity -= quantity;
+     
+            context.Orders.Remove(order);
+            context.SaveChanges();
+            return RedirectToAction("Record");
+
+        }
+
+        [Authorize(Roles = "BookOwner")]
+        public IActionResult Cancel(int id)
+        {
+            TempData["cancel"] = "Order has been cancelled";
+            var order = context.Orders.Find(id);
+            context.Orders.Remove(order);
+            context.SaveChanges();
+            return RedirectToAction("Record");
+
         }
     }
 }
